0d361fbbb1f8d4d647e7e4da6ff3837a6a0a350d
guillaumeingrin <guillaume.ingrin@gmail.com>
2023-11-28 14:33:07 +0100

    add column photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_132029) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_133124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_132029) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "likes", force: :cascade do |t|
