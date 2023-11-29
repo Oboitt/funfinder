d1a8f127e0c09e7511c0eca1a530c7676a7d9269
Oscar Boittiaux <oscar.boittiaux3@gmail.com>
2023-11-29 12:55:48 +0100

    map done

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,6 @@ class Activity < ApplicationRecord
   has_many :theme_activities
   has_many :themes, through: :theme_activities
   has_many :users, through: :likes
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_094211) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_102015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_094211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -71,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_094211) do
     t.string "first_name"
     t.string "last_name"
     t.integer "age"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
