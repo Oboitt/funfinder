181aa322906ded55ea47c64ccc58e3f876d1c406
guillaumeingrin <guillaume.ingrin@gmail.com>
2023-11-29 12:53:17 +0100

    activity ok

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,35 @@
 class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
+  before_action :set_activity, only: [:show]
 
   def index
     @activities = Activity.all
   end
 
   def show
+    @theme_activities = ThemeActivity.where(activity_id: @activity.id)
+    @likes = Like.where(activity_id: @activity.id)
+  end
+
+  # def create
+  #   @activity = Activity.new(activity_params)
+  #   if @activity.save
+  #   redirect_to activity_path(@activity)
+
+  # end
+
+  # def destroy
+  #   @activity.destroy
+  #   redirect_to activities_path
+  # end
+
+  private
+
+  # def activity_params
+  #   params.require(:activity).permit(:name, :brand, :address, :content, :date_begin, :date_end, :website, :photo)
+  # end
+
+  def set_activity
     @activity = Activity.find(params[:id])
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "glide", to: "https://ga.jspm.io/npm:glide@0.2.0/index.js", preload: true
+pin "tween.js", to: "https://ga.jspm.io/npm:tween.js@0.14.0/index.js", preload: true
