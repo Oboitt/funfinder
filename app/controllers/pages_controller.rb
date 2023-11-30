128a951f23da3d3e7f5d3c9df9dbed15b95e6325
guillaumeingrin <guillaume.ingrin@gmail.com>
2023-11-30 10:23:29 +0100

    homepage almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+     @activities = Activity.last(5)
      @activity = Activity.new
   end
 end
