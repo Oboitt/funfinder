de673afcc9a5320885c2d27cc30fa54a1b98afdb
guillaumeingrin <guillaume.ingrin@gmail.com>
2023-12-06 14:27:11 +0100

    test

--- app/javascript/controllers/activity_map_controller.js
+++ app/javascript/controllers/activity_map_controller.js
@@ -1,14 +1,14 @@
-import { Controller } from "@hotwired/stimulus";
+// import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {
-  click(event) {
-    event.preventDefault();
-    const markerId = this.element.dataset.activityMapMarkerId;
-    this.element.dispatchEvent(
-      new CustomEvent('activityCardClicked', {
-        bubbles: true,
-        detail: { markerId: markerId },
-      })
-    );
-  }
-}
+// export default class extends Controller {
+//   click(event) {
+//     event.preventDefault();
+//     const markerId = this.element.dataset.activityMapMarkerId;
+//     this.element.dispatchEvent(
+//       new CustomEvent('activityCardClicked', {
+//         bubbles: true,
+//         detail: { markerId: markerId },
+//       })
+//     );
+//   }
+// }
